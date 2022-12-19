bd3dc48c7ebaf45a21d78bd72e06776bcd879623
Galdino13 <59259616+Galdino13@users.noreply.github.com>
2022-12-19 08:11:28 -0300

    Add files via upload

---
+++ stylesDi.js
@@ -0,0 +1,106 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+
+
+const stylesDi = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FE51B0',
+  },container2: {
+    flex: 1,
+    marginTop:30,
+    justifyContent:"center",
+    backgroundColor: '#FE51B0',
+  },caixa1:{
+      flex: 1,
+      flexDirection:"row",
+  },subcaixa1:{
+    flex: 1,
+    flexDirection:"Column",
+  },subcaixa2:{
+      flex: 2,
+      flexDirection:"Column"
+  },caixa2:{
+    flex: 1,
+    flexDirection:"column",
+},subcaixa3:{
+  flex: 2,
+  flexDirection:"row",
+},subcaixa4:{
+    flex: 1,
+    flexDirection:"row"
+},
+  h1:{
+    fontFamily:"Segoe Ui",
+    fontSize:50,
+    fontWeight:'bold',
+    color:'white',
+    marginTop:40,
+    marginLeft:25,
+},p:{
+  color:'white',
+  fontSize:30,
+  fontFamily:'Segoe Ui',
+  fontWeight:'bold',
+  marginTop:20,
+  marginLeft:25,
+},infoTxt:{
+  fontFamily:"Segoe Ui",
+  color:"white",
+  fontSize:30,
+  marginTop:10,
+  marginLeft: 25,
+  textAlign:"left",
+},BtWord1:{
+    flex: 1,
+  backgroundColor:"#F5E45F",
+  textAlign:"center",
+  justifyContent: "center",
+}, BtWord2:{
+  flex: 2,
+  backgroundColor:"#00C69E",
+  textAlign:"center",
+  justifyContent: "center",
+},BtWord3:{
+    flex: 2,
+  backgroundColor:"#E2C1F3",
+  textAlign:"center",
+  justifyContent: "center",
+},BtWord4:{
+    flex: 1,
+  backgroundColor:"#FD7A39",
+  textAlign:"center",
+  justifyContent: "center",
+}, BtWord5:{
+    flex:2,
+  backgroundColor: "#FFBBE0",
+  textAlign:"center",
+  justifyContent: "center",
+},BtWord6:{
+    flex: 1,
+ 
+  backgroundColor:"#F5E45F",
+  textAlign:"center",
+  justifyContent: "center",
+},BtWord7:{
+    flex: 1,
+  backgroundColor:"#00C69E",
+  textAlign:"center",
+  justifyContent: "center",
+}, BtWord8:{
+    flex:1,
+  backgroundColor:"#FD7A39",
+  textAlign:"center",
+  justifyContent: "center",
+},word:{
+  fontFamily:"Segoe Ui",
+  color:'white',
+  textAlign:'center',
+  marginTop:15,
+  fontSize:25,
+  fontWeight:'bold',
+},
+});
+
+
+export default stylesDi;
